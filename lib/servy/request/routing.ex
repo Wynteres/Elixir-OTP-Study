7fcc9fe53192a9f8a6d1230716b9863dc991e79a
Raul Coan <rvc.coan@gmail.com>
2020-01-02 15:46:52 -0300

    separate handle file

--- lib/servy/request/routing.ex
+++ lib/servy/request/routing.ex
@@ -5,6 +5,8 @@ defmodule Servy.Request.Routing do
 
   @pages_path Path.expand("../../pages", __DIR__)
 
+  import Servy.FileHandler, only: [handle_file: 2]
+
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
@@ -44,17 +46,5 @@ defmodule Servy.Request.Routing do
 
   def route(%{method: method, path: path} = conv) do
     %{conv | status: 404, resp_body: "#{method} for #{path} not found."}
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found."}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 end
